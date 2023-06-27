e8ff49d440ec123dc7753f7efe8e3dac69b95d15
DarioMendoza <dm1289@gmail.com>
2023-06-27 15:56:23 -0600

    queueu manager base finished

--- __monitoring_engine/live_stream_status.py
+++ __monitoring_engine/live_stream_status.py
@@ -6,7 +6,7 @@ watch_list = []
 status_dir = "status"
 data_dir = "data"
 
-def generate_status(ignore_free_chat=True):
+def get_current_status(ignore_free_chat=True):
     in_f = open(f"{data_dir}/watch_list.txt", "r")
     
     freechat_list = []
@@ -17,25 +17,23 @@ def generate_status(ignore_free_chat=True):
             name, channel_id, video_id, *title = line.split(",")
             freechat_list.append(video_id)
 
-    print(freechat_list)
-
     timestamp = int(time.time())
     out_filename = f"{status_dir}/status.{timestamp}.txt"
     out_f = open(out_filename, "w", encoding="utf-8")
 
-    status = []
+    status = set()
     for line in in_f:
         channel_id, name = line.split(",")
         name = name.strip()
         video_id, video_url, title, description, keywords, scheduled_start_time = get_current_live_stream(channel_id)
-        print("loading", name, "current stream data...")
-        print(name, channel_id, video_id, scheduled_start_time, title, sep=",")
-        print(video_id in freechat_list)
+        #print("loading", name, "current stream data...")
+        #print(name, channel_id, video_id, scheduled_start_time, title, sep=",")
         if video_id != "-1" and video_id not in freechat_list:
-            status.append((name, channel_id, video_id, title))
+            status.add((name, channel_id, video_id, title))
             print(name, channel_id, video_id, scheduled_start_time, f'"{keywords}"',f'"{title}"', sep=",", file=out_f)
 
     return status
 
         
-generate_status()
+if __name__ == "__main__":
+    print(get_current_status(False))
--- __monitoring_engine/queue_manager.py
+++ __monitoring_engine/queue_manager.py
@@ -1,8 +1,51 @@
 import os
 import time
+import threading, queue
+from live_stream_status import get_current_status
 
-while True:
-    
-    
+def worker(name):
+    print("w", name, "init...")
+    while True:
+        item = q.get()
+        print("w", name, "started task...")
+        ####
 
-    time.sleep(15)
+        print("w", w, ":", item)
+        time.sleep(5)
+
+        active_video_list.remove(item)
+        ###
+        print("w", name, "finished task...")
+        q.task_done()
+
+
+alive = True
+active_video_list = set()
+q = queue.Queue()
+worker_amount = 10
+
+# thread_list = []
+for w in range(worker_amount):
+    threading.Thread(target=worker, daemon=True, args=(w,)).start()
+
+try:
+    while alive:
+        print("checking current status...")
+
+        ongoing_streams = get_current_status(False)
+        video_id = ""
+        print("found:", len(ongoing_streams - active_video_list))
+
+        for t in ongoing_streams:
+            channel_video, channel_id, video_id, video_name = t
+            if t not in active_video_list:
+                q.put(t)
+                active_video_list.add(t)
+
+        time.sleep(15)
+except KeyboardInterrupt:
+    alive = False
+
+q.join()
+
+print("Finished")
